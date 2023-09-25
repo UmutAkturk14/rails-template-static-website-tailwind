3cc83292a0d08587f122876f307782dd77320715
Umut Akturk <umut.akturk14@gmail.com>
2023-09-25 20:06:08 +0300

    Fixed a couple of issues

--- db/schema.rb
+++ db/schema.rb
@@ -27,5 +27,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_230722) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
